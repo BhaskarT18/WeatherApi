2a0981b819734e927cacc9c0c6e28b7b45e15b1f
BhaskarT18 <bhaskartikale@gmail.com>
2024-08-01 00:05:19 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const express= require("express");
+const https=require("https");
+const bodyPaser=require("body-parser")
+const app=express();
+
+app.use(bodyPaser.urlencoded({extended:true}));
+
+
+app.get("/",function(req,res){
+ res.sendFile(__dirname+ "/index.html");
+});
+
+app.post("/", function(req ,res){
+
+ const query= req.body.cityName;
+const uniKey="1aef0c04b8e203884585843cfff2c73c";
+const unit="metric";
+
+const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+uniKey+"&units="+unit;
+https.get(url,function(response){
+    console.log(response.statusCode);
+
+    response.on("data",function(data){
+        const weatherData= JSON.parse(data);
+       const temp =weatherData.main.temp;
+       const weatherDecrip= weatherData.weather[0].description;
+       const icon= weatherData.weather[0].icon; 
+       const imgUrl="https://openweathermap.org/img/wn/"+icon+"@2x.png"
+       res.write("<p>The current description is   "  +weatherDecrip+"  </p>");
+       res.write("<h1>The current temprature in "+query + "    is "  +  temp +"  degree celcius </h1>")
+       res.write("<img src="+ imgUrl+ ">");
+       res.send();
+
+    });
+
+});
+   
+
+})
+
+
+
+ 
+app.listen(3000,function(){
+    console.log("The server is running ");
+});
